1db84106ef7f021c95ac53c70a8ab6d8792d378c
Bram Hagens <bramhagens@hotmail.nl>
2017-06-25 00:46:31 +0200

    Fixed #23

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -1,3 +1,5 @@
+-ignorewarnings
+
 # Glide #
 -keep public class * implements com.bumptech.glide.module.GlideModule
 -keep public enum com.bumptech.glide.load.resource.bitmap.ImageHeaderParser$** {
--- app/src/main/kotlin/me/bramhaag/owoandroid/activities/SettingsActivity.kt
+++ app/src/main/kotlin/me/bramhaag/owoandroid/activities/SettingsActivity.kt
@@ -1,7 +1,10 @@
 package me.bramhaag.owoandroid.activities
 
+import android.content.Intent
 import android.preference.PreferenceFragment
+import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.app.AppCompatDelegate
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: android.os.Bundle?) {
@@ -13,6 +16,14 @@ class SettingsActivity : AppCompatActivity() {
         fragmentManager.beginTransaction()
                 .replace(android.R.id.content, SettingsFragment())
                 .commit()
+
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener({ prefs, key ->
+            if(key == "pref_dark_theme") {
+                AppCompatDelegate.setDefaultNightMode(if (prefs.getBoolean("pref_dark_theme", false)) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO)
+                startActivity(Intent(this, SettingsActivity::class.java))
+                finish()
+            }
+        })
     }
 
     class SettingsFragment : PreferenceFragment() {
